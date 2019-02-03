8e589aaa5fc1ddcb11581cd20f60f321d7781e8e
suwei.air <suwei.air@gmail.com>
2019-02-03 17:57:41 +0800

    ignore installed pip package

--- .travis/install.sh
+++ .travis/install.sh
@@ -22,8 +22,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pip install conan_package_tools
 
 else
-    sudo pip install conan --upgrade
-    sudo pip install conan_package_tools
+    sudo pip install conan conan_package_tools --ignore-installed
 
 fi
 
