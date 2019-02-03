22dbe7ff11ba21c951163dbc7f6afc53dae4ca50
suwei.air <suwei.air@gmail.com>
2019-02-03 20:49:05 +0800

    build debug & release in same package

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 from conans import ConanFile, CMake
 
 
@@ -12,7 +12,7 @@ class PocoConan(ConanFile):
         "The POCO C++ Libraries are powerful cross-platform C++ libraries "
         "for building network- and internet-based applications "
         "that run on desktop, server, mobile, IoT, and embedded systems.")
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "arch"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
@@ -74,15 +74,23 @@ class PocoConan(ConanFile):
             elif not option_name == "fPIC":
                 cmake.definitions[option_name.
                                   upper()] = "ON" if activated else "OFF"
-        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":  # MT or MTd
-            cmake.definitions["POCO_MT"] = "ON" if "MT" in str(
-                self.settings.compiler.runtime) else "OFF"
         self.output.info(cmake.definitions)
         os.mkdir(".build")
-        cmake.configure(
-            source_folder=os.path.join(self.source_folder, "poco"),
-            build_folder=".build")
-        cmake.build()
+        # Build debug & release
+        if cmake.is_multi_configuration:
+            cmake.configure(source_folder="poco", build_folder=".build")
+            for config in ("Debug", "Release"):
+                self.output.info("Building {}".format(config))
+                cmake.build_type = config
+                cmake.build()
+        else:
+            for config in ("Debug", "Release"):
+                self.output.info("Building {}".format(config))
+                cmake.build_type = config
+                cmake.configure(source_folder="poco", build_folder=".build")
+                cmake.build()
+                shutil.rmtree("CMakeFiles")
+                os.remove("CMakeCache.txt")
 
     def package(self):
         # Copy the license files
@@ -99,7 +107,9 @@ class PocoConan(ConanFile):
             packages.append("NetSSL_OpenSSL")
         for header in packages:
             self.copy(
-                pattern="*.h", dst="include", src="poco/%s/include" % header)
+                pattern="*.h",
+                dst="include",
+                src="poco/{}/include".format(header))
         # But for libs and dlls, we want to avoid intermediate folders
         self.copy(
             pattern="*.lib", dst="lib", src=".build/lib", keep_path=False)
@@ -116,6 +126,14 @@ class PocoConan(ConanFile):
         self.copy(
             pattern="*.dylib*", dst="lib", src=".build/lib", keep_path=False)
 
+    def _append_lib(self, lib):
+        if self.settings.compiler == "Visual Studio" and not self.options.shared:
+            suffix = ["mdd", "md"]
+        else:
+            suffix = ["d", ""]
+        self.cpp_info.debug.libs.append("{}{}".format(lib, suffix[0]))
+        self.cpp_info.release.libs.append("{}{}".format(lib, suffix[1]))
+
     def package_info(self):
         libs = [("enable_mongodb", "PocoMongoDB"), ("enable_pdf", "PocoPDF"),
                 ("enable_net", "PocoNet"), ("enable_netssl", "PocoNetSSL"),
@@ -128,15 +146,12 @@ class PocoConan(ConanFile):
                 ("enable_apacheconnector", "PocoApacheConnector"),
                 ("enable_util", "PocoUtil"), ("enable_xml", "PocoXML"),
                 ("enable_json", "PocoJSON"), ("enable_redis", "PocoRedis")]
-        suffix = str(self.settings.compiler.runtime).lower()  \
-                 if self.settings.compiler == "Visual Studio" and not self.options.shared \
-                 else ("d" if self.settings.build_type=="Debug" else "")
         for flag, lib in libs:
             if getattr(self.options, flag):
                 if self.settings.os != "Windows" and flag == "enable_netssl_win":
                     continue
-                self.cpp_info.libs.append("%s%s" % (lib, suffix))
-        self.cpp_info.libs.append("PocoFoundation%s" % suffix)
+                self._append_lib(lib)
+        self._append_lib("PocoFoundation")
         # in linux we need to link also with these libs
         if self.settings.os == "Linux":
             self.cpp_info.libs.extend(["pthread", "dl", "rt"])
