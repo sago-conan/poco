4588bd6fe9b75a5b1b863ec66b412758d55e0649
suwei.air <suwei.air@gmail.com>
2019-02-03 20:05:50 +0800

    narrow builds

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 from cpt.packager import ConanMultiPackager
 
-
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="Poco:shared", pure_c=False)
+    builder.remove_build_if(
+        lambda build: ("compiler.libcxx" in build.settings and build.settings["compiler.libcxx"] == "libstdc++") or ("compiler.runtime" in build.settings and build.settings["compiler.runtime"].startswith("MT"))
+    )
     builder.run()
